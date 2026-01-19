87fc296547a02bf43e946acadf74b5cf62af5b39
Nicholas L. S. Silva <nicholaslima.rw@gmail.com>
2026-01-18 22:15:21 -0300

    Bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = readme_file.read_text(encoding="utf-8") if readme_file.exists
 
 setup(
     name="witcherscript",
-    version="1.1.0",
+    version="1.1.1",
     author="WitcherScript Contributors",
     description="A programming language inspired by The Witcher 3: Wild Hunt",
     long_description=long_description,
